b55738ebc486889b3369ca931b6c149f7a95950a
riteshkrwl <ritesh.kumar18@wipro.com>
2022-07-15 17:11:00 +0530

    adding playgound documentation

---
+++ JulyMyPlayground.playground/Pages/Swift_Documentation.xcplaygroundpage/Contents.swift
@@ -0,0 +1,138 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//: # SWIFT DOCUMENTATION HEADER
+
+//: SINGLE LINE text for documentation.
+/// Here also some onle line text
+/*:
+MULTI-LINE TEXT
+
+ Here is the multi-line text that you want to write
+ for your documentation/description for the programming.
+ It will be shown in a text box while preview in render markup view under
+ the "SHOW RENDERED MARKUP "
+ */
+
+// user #<Space>for the Header
+
+//: # Header 1
+//: ## Header 2
+//: ### Header 3
+//: #### Header 4
+
+
+//: Level 1 Heading Using Equals Sign ('=')
+//: =
+
+/*: another header is like below
+ =
+ */
+
+/*:
+**Bold text**
+
+__Another Bold Text__
+
+*Italic text*
+
+_Another Italic Text_
+
+ */
+
+/*:
+ Insert a horizontal rule by using three or more hyphens (-), asterisks (*), or underscores (_) on a line that is surrounded by empty lines. Each of the characters is the same for a horizontal rule. Whitespace between the characters is ignored
+ 
+ - - - -
+ * * * *
+ _ _ _ _
+ */
+
+
+/*: List with the bulleted Items
+  - Item 1, List A
+  - Item 2, List A
+  - Item 3, List A
+  - Item 4, List A
+  + Item1, List B
+  + Item2, List B
+  + Item3, List B
+  + Item4, List B
+  - - - -
+ List with Numbered Items
+ 1. Item 1
+ 2. Item 2
+ 3. Item 3
+    1. item 3,1
+    2. item 3,2
+    3. item 3,3
+    4. item 3,4
+ 4. Item 4
+  - - - -
+ `this is monospaced text `
+
+ - - - -
+*/
+/// [tesxt for URL](URL)
+//:[Click to see more details](https://developer.apple.com/library/archive/documentation/Xcode/Reference/xcode_markup_formatting_ref/Links.html#//apple_ref/doc/uid/TP40016497-CH18-SW1)
+ 
+
+/*:Check for examples:
+ 
+    this is and expamle with a balnek line above,
+    followed by the tab for each line to be included in example span.
+ */
+
+/*:
+ _ _ _ _ _ _ _ _ _ _ _
+ 
+ 
+ - Callout(Llama Spotting Tips):
+   Pack warm clothes with your binoculars.
+ */
+
+
+/*:
+ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+ 
+  - Example: *A simple `for` loop.*\
+  This example shows a `for` loop that prints the numbers 1 to 5.\
+ \
+  `for index in 1...5 {`\
+  `   print("index = \(index)")`\
+  `}`}
+ */
+
+/*:
+ 
+ _ _ _ _ _ _
+ 
+  * Experiment:
+  Find out if you are tasting your food or smelling it.
+ 
+     1. Get a few different foods you like.
+     2. Cut them up so they are all about the same size.
+     3. Put them in a dish and mix them up.
+     4. Close your eyes (no peeking!)
+     5. Hold you nose and eat several pieces of food.
+ 
+     Could you tell which food was which?
+ */
+
+/*:
+  - Important:
+  "The beginning is the most important part of the work."
+  \
+  –Plato
+ */
+
+/*:
+  - Note:
+   "There is nothing either good or bad, but thinking makes it so."
+  \
+  \
+  Hamlet in (*Hamlet, 2.2*) by William Shakespeare
+*/
+
+ //: [Next](@next)
